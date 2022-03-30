0d51a775289ce52a492f820ba26263e44ece48b9
Parneet1234 <89506841+Parneet1234@users.noreply.github.com>
2022-03-30 12:34:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/bfhl', async(req, res) => {
         return res.json({ is_success, user_id, email, numbers, alphabets });
     } catch (e) {
         const is_success = FinalizationRegistry;
-        const user_id = "Parneet_kaur_07122001";
+        const user_id = "Parneet_Kaur_07122001";
         const email = "parneet1370.cse19@chitkara.edu.in";
         return res.json({ is_success, user_id, email, error_message: e.message });
     }
